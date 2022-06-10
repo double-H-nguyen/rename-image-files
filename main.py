7111302ac3c8612b224fdf5d0e0a6dcc3282dc7d
Henry Nguyen <nguyenhenryh1996@gmail.com>
2022-06-09 19:28:28 -0500

    Extract exif data

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+from exif import Image
+
 def main():
     # ask user to input directory of images
-    file = "20160712_174018.jpg"
+    #file_path = input("Copy and paste the directory in which you want the script to execute in: ")
+    file_path = "20160712_174018.jpg" # test only
+
+    # Read binary of image file and instantiate as exif Image object
+    with open(file_path, "rb") as file:
+        image = Image(file)
+
+    # if image does not have exif data, skip image (don't rename)
+    #if not image.has_exif:
+        #continue
 
     # extract EXIF data from image
+    image_datetime, image_make, image_model, image_gps_lat, image_gps_lat_ref, image_gps_long, image_gps_long_ref = get_exif_data(image)
+    print(image_datetime, image_make, image_model, image_gps_lat, image_gps_lat_ref, image_gps_long, image_gps_long_ref)
 
     # convert GPS coord from DMS to DD
 
@@ -10,6 +23,9 @@ def main():
 
     # rename file
 
+# extract exif data from image and package into list
+def get_exif_data(image):
+    return [image.datetime_original, image.make, image.model, image.gps_latitude, image.gps_latitude_ref, image.gps_longitude, image.gps_longitude_ref]
 
 if __name__ == "__main__":
     main()
