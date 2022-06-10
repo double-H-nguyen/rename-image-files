40c0604266d98cfa16e90fbcc8a292ff85b8aa6f
Henry Nguyen <nguyenhenryh1996@gmail.com>
2022-06-09 20:45:32 -0500

    find address using coordinates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from exif import Image
+from geopy.geocoders import Nominatim
 
 def main():
     # ask user to input directory of images
@@ -20,8 +21,10 @@ def main():
     latitude, longitude = DMS_to_DD(image_gps_lat, image_gps_lat_ref, image_gps_long, image_gps_long_ref)
 
     # get address from DD
+    road, city, state, country = get_address(latitude, longitude)
 
     # rename file
+    print(f"{image_datetime}_{road}_{city}_{state}_{country}_{image_make}_{image_model}")
 
 
 # extract exif data from image and package into list
@@ -41,6 +44,19 @@ def DMS_to_DD(latitude_DMS, latitude_ref, longitude_DMS, longitude_ref):
     longitude_DD = (d + m / 60.0 + s / 3600.0) * (-1 if longitude_ref == "W" else 1)
 
     return [latitude_DD, longitude_DD]
+
+
+# given latitude and longitude in DD, return road, city, state, and country
+def get_address(latitude, longitude):
+    coord_decimal = f"{latitude}, {longitude}"
+    geolocator = Nominatim(user_agent="http")
+    location = geolocator.reverse(coord_decimal)
+
+    road = location.raw.get('address').get('road').replace(" ", "")
+    city = location.raw.get('address').get('city').replace(" ", "")
+    state = location.raw.get('address').get('state').replace(" ", "")
+    country = location.raw.get('address').get('country').replace(" ", "")
+    return [road, city, state, country]
 
 
 if __name__ == "__main__":
