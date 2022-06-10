7e8ab6e4204f4bf876dde447ca10705c8b1f58c6
Henry Nguyen <nguyenhenryh1996@gmail.com>
2022-06-09 19:06:21 -0500

    add script outline

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    # ask user to input directory of images
+    file = "20160712_174018.jpg"
+
+    # extract EXIF data from image
+
+    # convert GPS coord from DMS to DD
+
+    # get address from DD
+
+    # rename file
+
+
+if __name__ == "__main__":
+    main()
